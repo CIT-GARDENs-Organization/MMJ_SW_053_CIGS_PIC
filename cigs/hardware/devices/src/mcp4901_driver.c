c8bb1253a7e5532f51a5b4e6a7157eb421e61911
sho-ino <sho.inoue.7735@gmail.com>
2025-07-27 19:47:22 +0900

    フォルダ構造微修正

--- cigs/hardware/devices/src/mcp4901_driver.c
+++ cigs/hardware/devices/src/mcp4901_driver.c
@@ -5,13 +5,13 @@
 void mcp4901_init()
 {
     fprintf(PC, "MCP4901 Initialize\r\n");
-    output_high(DAC1_CS); // Set CS pin high to deselect the DAC
-    output_high(DAC2_CS); // Set CS pin high to deselect the second DAC
-    delay_ms(10); // Wait for the DAC to stabilize
-    mcp4901_1_write(0); // Initialize DAC 1 to 0
-    mcp4901_2_write(0); // Initialize DAC 2 to 0
+    output_high(DAC1_CS); 
+    output_high(DAC2_CS); 
+    delay_ms(10); 
+    mcp4901_1_write(0); 
+    mcp4901_2_write(0); 
     unsigned int16 readdata;
-    readdata = ad7490_read(ADC_CIGS1_DAC); // Read a dummy value to initialize the DAC
+    readdata = ad7490_read(ADC_CIGS1_DAC); 
     fprintf(PC, "%04LX\r\n", readdata);
     if (readdata == 0xFFFF || readdata == 0x0000) {
         fprintf(PC, "\t[DAC Port 1] Connect error!\r\n");
@@ -19,7 +19,7 @@ void mcp4901_init()
         fprintf(PC, "\t[DAC Port 1] Connect successful\r\n");
     }
     fprintf(PC, "%04LX\r\n", readdata);
-    readdata = ad7490_read(ADC_CIGS2_DAC); // Read a dummy value to initialize the second DAC
+    readdata = ad7490_read(ADC_CIGS2_DAC); 
     if (readdata == 0xFFFF || readdata == 0x0000) {
         fprintf(PC, "\t[DAC Port 2] Connect error!\r\n");
     } else {
@@ -35,10 +35,10 @@ void mcp4901_1_write(unsigned int16 value)
    #ifdef MCP4901_DEBUG
       fprintf(PC, "\t[DAC] <<< %04LX\r\n", cmd);
    #endif
-   output_low(DAC1_CS); // Select MCP4901
-   delay_us(100); // Ensure CS is stable before sending data
-   spi_xfer(ADCDAC_STREAM, cmd); // Send first byte
-   output_high(DAC1_CS); // Deselect MCP4901
+   output_low(DAC1_CS); 
+   delay_us(10); 
+   spi_xfer(ADCDAC_STREAM, cmd); 
+   output_high(DAC1_CS); 
 }
 
 void mcp4901_2_write(unsigned int16 value)
@@ -47,9 +47,9 @@ void mcp4901_2_write(unsigned int16 value)
    #ifdef MCP4901_DEBUG
       fprintf(PC, "\t[DAC] <<< %04LX\r\n", cmd);
    #endif
-   output_low(DAC2_CS); // Select MCP4901
-   delay_us(100); // Ensure CS is stable before sending data
-   spi_xfer(ADCDAC_STREAM, cmd); // Send first byte
-   output_high(DAC2_CS); // Deselect MCP4901
+   output_low(DAC2_CS); 
+   delay_us(10); 
+   spi_xfer(ADCDAC_STREAM, cmd); 
+   output_high(DAC2_CS); 
 }
 
